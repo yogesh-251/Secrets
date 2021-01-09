fc819c168847282faf83937204b4169a375b5ae9
Yogesh <singhaniayogesh007@gmail.com>
2021-01-10 00:09:16 +0530

    Change in Procfile

--- app.js
+++ app.js
@@ -161,7 +161,7 @@ app.post("/login", function(req, res){
 });
 
 let port = process.env.PORT;
-if(port== null || port == ""){
+if(port == null || port == ""){
   port = 3000;
 }
 app.listen(port, function(){
